eac40d4801fd3446ff631c18b12e6146231a69e7
prabodha <prabodhasenadhipathi123@gmail.com>
2022-07-21 11:48:25 +0530

    new file:   .classpath 	new file:   .project 	new file:   .settings/.jsdtscope 	new file:   .settings/org.eclipse.jdt.core.prefs 	new file:   .settings/org.eclipse.m2e.core.prefs 	new file:   .settings/org.eclipse.wst.common.component 	new file:   .settings/org.eclipse.wst.common.project.facet.core.xml 	new file:   .settings/org.eclipse.wst.jsdt.ui.superType.container 	new file:   .settings/org.eclipse.wst.jsdt.ui.superType.name 	new file:   .settings/org.eclipse.wst.validation.prefs 	new file:   WebContent/META-INF/MANIFEST.MF 	new file:   WebContent/WEB-INF/web.xml 	new file:   WebContent/index.jsp 	new file:   pom.xml 	new file:   src/com/prabodha/pro/Register.java 	new file:   target/classes/com/prabodha/pro/Register.class 	new file:   target/m2e-wtp/web-resources/META-INF/MANIFEST.MF 	new file:   target/m2e-wtp/web-resources/META-INF/maven/Servletproject/Servletproject/pom.properties 	new file:   target/m2e-wtp/web-resources/META-INF/maven/Servletproject/Servletproject/pom.xml

---
+++ SevletProject/Servletproject/WebContent/index.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>Welcome Users</h1>
+
+<form action="register" method ="post">
+
+Enter the first name:<input type ="text" name ="firstname" />
+<br><br>
+Enter the last name: <input type ="text" name ="lastname" />
+<br><br>
+Enter the mobile: <input type ="text" name ="mobile" />
+<input type ="submit" value ="submit"/>
+
+</form>
+
+
+</body>
+</html>
---
+++ SevletProject/Servletproject/src/com/prabodha/pro/Register.java
@@ -0,0 +1,52 @@
+package com.prabodha.pro;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Register
+ */
+@WebServlet("/register")
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
+		String mobile = request.getParameter("mobile");
+		
+		PrintWriter writer = response.getWriter();
+		 writer.println(firstname +" "+lastname);
+		 writer.println(" Mobile " + mobile);
+		 writer.flush();
+		 
+//		doGet(request, response);
+	}
+
+}
